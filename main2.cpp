5fc64eb8a77f7ba92d652715fca9966d8f7c9d6a
pratapshingane18 <sushrutrao465@gmail.com>
2023-04-16 10:33:16 +0530

    main2

---
+++ main2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int output(){
+    output();
+    cout<<"Recurrsion"<<endl;
+return 0;
+
+}
+
+int main(){
+    output();
+    return 0;
+}
+
+// Output:
+
+// It will print Nothing 
+// It will go into infinite function call and then
+// Segmentation error will occur

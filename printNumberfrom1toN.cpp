feb2eca207a78860d1f3f4faa78d61749a0d556a
pratapshingane18 <sushrutrao465@gmail.com>
2023-04-16 11:18:02 +0530

    print num 1 to  n times

---
+++ printNumberfrom1toN.cpp
@@ -0,0 +1,20 @@
+//Print number from 1 to N
+#include<iostream>
+using namespace std;
+int cnt =1;
+int output(int n){
+    if(cnt > n){
+        return 0;
+    }
+    cout<<cnt<<endl;
+    cnt++;
+    output(n);
+
+return 0;
+
+}
+
+int main(){
+    output(100);
+    return 0;
+}
